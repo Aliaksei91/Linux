90cfbd3b8c7a27e4eaaaef969e866c04e374f2f2
Aliaksei Zhibul <bbsacredaa@gmail.com>
2021-04-28 22:13:02 +0300

    fix script

--- linux_script.sh
+++ linux_script.sh
@@ -5,5 +5,5 @@ cd folder2.1
 touch 1.txt 2.txt 3.txt 4.json 5.json
 mkdir f1 f2 f3
 ls -l
-mv 4.json 5.json /c/Users/user/gir_repo/Linux/folder2/folder2.2
+mv 4.json 5.json /c/Users/User/gir_repo/Linux/folder2/folder2.2
 ls -l
